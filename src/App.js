824947b982f23c1f096ed750c5e23e775ad544de
Thomas Ehmann <ehmannth@gmail.com>
2019-09-13 21:19:12 +0200

    Add React Router Setup

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-      <Recipes></Recipes>
-      <SingleRecipe></SingleRecipe>
-      <Default></Default>
-    </div>
-  )
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/recipes" exact component={Recipes}></Route>
+          <Route path="/recipes/:id" component={SingleRecipe}></Route>
+          <Route component={Default}></Route>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
