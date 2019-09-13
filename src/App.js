be1e952a3321a9befad763fc18933906ac12e307
Thomas Ehmann <ehmannth@gmail.com>
2019-09-13 21:59:49 +0200

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
       <main>
+        <Navbar></Navbar>
         <Switch>
           <Route path="/" exact component={Home}></Route>
           <Route path="/recipes" exact component={Recipes}></Route>
