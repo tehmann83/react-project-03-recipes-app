8e873b835e7ff158ced3bda815615c0d29ce9158
Thomas Ehmann <ehmannth@gmail.com>
2019-09-15 18:09:09 +0200

    Added Recipe component

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Recipe extends Component {
   render() {
-    const {recipe} = this.props;
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe;
     return (
-      <div>
-        {recipe.title}
+      <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+        <div className="card" style={{ height: "100%" }}>
+          <img
+            src={image_url}
+            style={{ height: "14rem" }}
+            alt="recipe"
+            className="img-card-top"
+          />
+          <div className="card-body-text-capitalize">
+            <h6>{title}</h6>
+            <span>
+              <h6 className="">provided by</h6>
+              <h6 className="text-danger text-slanted">{publisher}</h6>
+            </span>
+            <div className="card-footer">
+              <Link
+                to={`/recipes/${recipe_id}`}
+                className="btn btn-primary text-capitalize"
+              >
+                details
+              </Link>
+              <a
+                href={source_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-success mx-2 text-capitalize"
+              >
+                go to recipe
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
