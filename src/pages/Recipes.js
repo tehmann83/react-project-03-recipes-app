aafacb297a61a4c8266273aa217e95549e0f8595
Thomas Ehmann <ehmannth@gmail.com>
2019-09-16 09:18:03 +0200

    Added search functionality

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -11,8 +11,11 @@ export default class Recipes extends Component {
 
   state = {
     recipes: recipeData,
+    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+    query: "&q=",
     search: "",
-    url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`
+    error: ""
   };
 
   async getRecipes() {
@@ -20,7 +23,15 @@ export default class Recipes extends Component {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
       console.log(jsonData);
-      this.setState({ recipes: jsonData.recipes });
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "Couldn't find any recipes. \nPlease try again or press the search icon for the most popular recipes.",
+          search: ""
+        });
+      } else {
+        this.setState({ recipes: jsonData.recipes, error: "" });
+      }
     } catch (error) {
       console.log(error);
     }
@@ -39,6 +50,13 @@ export default class Recipes extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`
+      },
+      () => this.getRecipes()
+    );
   };
 
   render() {
@@ -49,7 +67,19 @@ export default class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         ></Search>
-        <RecipeList recipes={this.state.recipes}></RecipeList>
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes}></RecipeList>
+        )}
       </>
     );
   }
