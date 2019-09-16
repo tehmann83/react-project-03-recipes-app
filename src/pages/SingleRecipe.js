700f4847a3f8c804a52e91b93ed35d61875e8a5e
Thomas Ehmann <ehmannth@gmail.com>
2019-09-16 08:46:16 +0200

    Added Recipe page ajax request

--- src/pages/SingleRecipe.js
+++ src/pages/SingleRecipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { recipeData } from "../data/tempDetails";
+//import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
 
 export default class SingleRecipe extends Component {
